25b94f530b8885ffa4443b06207e3cdeab98ed0c
jguilhermecoelho <jose.guilherme.oliveira@ccc.ufcg.edu.br>
2021-01-28 11:44:46 -0300

    Projeto Finalizado

---
+++ src/main/java/com/ufcg/psoft/enumeration/SituacaoProduto.java
@@ -0,0 +1,13 @@
+package com.ufcg.psoft.enumeration;
+
+public enum SituacaoProduto {
+
+    DISPONIVEL("Disponivel"),
+    INDISPONIVEL("Indisponivel");
+
+    public final String situacaoProduto;
+
+    private SituacaoProduto(String situacaoProduto) {
+        this.situacaoProduto = situacaoProduto;
+    }
+}
---
+++ src/main/java/com/ufcg/psoft/enumeration/StatusCompra.java
@@ -0,0 +1,13 @@
+package com.ufcg.psoft.enumeration;
+
+public enum StatusCompra {
+
+    ABERTA("em aberto"),
+    FECHADA("fechada");
+
+    public final String statusCompra;
+
+    private StatusCompra(String statusCompra) {
+        this.statusCompra = statusCompra;
+    }
+}
